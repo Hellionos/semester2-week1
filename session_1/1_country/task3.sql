63fe33395d7c14ded67b873daaea5aa78b6fc8ad
Ethan West <sqqz0154@leeds.ac.uk>
2026-01-26 12:55:27 +0000

    Half way through task 3

--- session_1/1_country/task3.sql
+++ session_1/1_country/task3.sql
@@ -12,4 +12,15 @@
 
 -- write your sql code here
 
-SELECT * from countries;
+SELECT COUNT(Country) FROM countries;
+SELECT COUNT(Country) FROM countries GROUP BY Continent;
+SELECT SUM(Population) FROM countries GROUP BY Continent;
+
+
+--4. Find the average GDP per capita for each continent.
+--5. Find continents that have more than 5 countries.
+--6. Find continents where the total population is greater than 1 billion.
+--7. Find the country with the highest population in each continent.
+--8. For each continent, find the average population density, the highest population, and the lowest GDP per capita.
+--9. Find continents where the average population density is greater than 100 and the total GDP per capita is greater than 5000.
+--10. For each continent, calculate the total area, the total arable percentage, and the average birthrate for countries with a population greater than 10 million.
