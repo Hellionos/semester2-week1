7ba7a2d1dfae0fe97e0f881eae47de3317125049
Ethan West <sqqz0154@leeds.ac.uk>
2026-01-26 12:33:16 +0000

    done task 2 of exercise 1

--- session_1/1_country/task2.sql
+++ session_1/1_country/task2.sql
@@ -12,17 +12,16 @@
 
 -- write your sql code here
 
-SELECT Country from countries ORDER BY Population DES LIMIT 10;
+SELECT Country from countries ORDER BY Population DESC LIMIT 10;
+SELECT Country from countries ORDER BY AreaSqMi ASC LIMIT 5;
+SELECT Country from countries WHERE Continent='Europe' ORDER BY AreaSqMi DESC LIMIT 1;
+SELECT Country from countries WHERE Continent='Africa' ORDER BY AreaSqMi ASC LIMIT 3;
+SELECT Country from countries ORDER BY LiteracyPercent ASC LIMIT 5;
+SELECT Country from countries WHERE Country LIKE 'C%' ORDER BY GDPPerCapita DESC LIMIT 3;
+SELECT Country from countries WHERE InfantMortalityPer1000>50 ORDER BY LiteracyPercent DESC LIMIT 1;
+SELECT Country from countries WHERE GDPPerCapita>10000 ORDER BY PhonesPer1000 ASC LIMIT 5;
+SELECT Country from countries WHERE Continent='South America' AND CoastlineRatio = 0 ORDER BY GDPPerCapita DESC LIMIT 1;
+SELECT Country from countries ORDER BY Country DESC LIMIT 10;
 
--- Exercises
---1. The 10 most populated countries
---2. The smallest 5 countries by area.
---3. The largest country in Europe
---4. The smallest 3 countries in Africa
---5. The 5 countries with the lowest literacy rate.
---6. The 3 richest (by GDP) countries beginning with ‘C’.
---7. The country with the highest literacy rate with an infant mortality rate above 50.
---8. The 5 countries with the lowest phones per 1000 population with a GDP above 10000.
---9. The richest landlocked country in South America.
---10. The last 10 countries alphabetically.
+
 
