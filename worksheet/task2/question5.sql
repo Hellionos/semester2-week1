3b8e8598689ddfa9f6a81b54cd52147a0c7cbd2d
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-01 10:35:14 +0000

    try this for q5

--- worksheet/task2/question5.sql
+++ worksheet/task2/question5.sql
@@ -5,6 +5,6 @@
 --Ethan West
 --201990893
  
-SELECT Student.StudentId AS StudentId, FirstName, LastName, SUM(Enrolment.Grade) AS TotalCreditsPassed 
+SELECT Student.StudentId AS StudentId, FirstName, LastName, SUM(Course.Credits) AS TotalCreditsPassed 
 FROM Student JOIN Enrolment  ON Student.StudentId=Enrolment.StudentId 
 JOIN Course ON Enrolment.CourseId=Course.CourseId WHERE Enrolment.Grade>=40 GROUP BY Student.StudentId;
