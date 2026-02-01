b1a76883253e4ebd5809d54481d60034b642298f
Ethan West <sqqz0154@leeds.ac.uk>
2026-02-01 09:57:42 +0000

    done q3

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -4,3 +4,5 @@
 
 --Ethan West
 --201990893
+
+SELECT DepartmentName, COUNT(EnrolmentId) AS TotalEnrolments FROM Department JOIN Course ON Department.DepartmentId=Course.DepartmentId JOIN Enrolment ON Course.CourseId=Enrolment.CourseId GROUP BY Department.DepartmentId;
